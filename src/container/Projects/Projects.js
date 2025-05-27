61cc2661ea1ae5213658d89c39cb7390b2c46701
Adeolu David <adeolusegun99@gmail.com>
2025-05-27 15:28:18 +0100

    added ios for tabs

--- src/container/Projects/Projects.js
+++ src/container/Projects/Projects.js
@@ -34,27 +34,27 @@ const Projects = () => {
   };
   return (
     <>
-      <h2 className="head-text">
+      <h2 className='head-text'>
         Projects:
         <span> Collaborative</span>
         <span> And </span>
         Individual
       </h2>
-      <div className="app__work-filter">
+      <div className='app__work-filter'>
         {[
-          "Native Android",
-          "Native IOS",
-          "Cross-Platform Mobile",
-          "Web Apps",
-          "Backend As A Service",
-          "Android Games",
-          "All",
+          'Native Android',
+          'Native iOS',
+          'Cross-Platform Mobile',
+          'Web Apps',
+          'Backend As A Service',
+          'Android Games',
+          'All',
         ].map((item, index) => (
           <div
             key={index}
             onClick={() => handleWorkFilter(item)}
             className={`app__work-filter-item app__flex p-text ${
-              activeFilter === item ? "item-active" : ""
+              activeFilter === item ? 'item-active' : ''
             }`}
           >
             {item}
@@ -65,61 +65,61 @@ const Projects = () => {
       <motion.div
         animate={animateCard}
         transition={{ duration: 1, delayChildren: 1 }}
-        className="app__work-projects"
+        className='app__work-projects'
       >
         {filterWork.map((work, index) => (
-          <div className="app__work-item app__flex" key={index}>
-            <div className="app__work-img app__flex">
+          <div className='app__work-item app__flex' key={index}>
+            <div className='app__work-img app__flex'>
               <img src={urlFor(work.imgUrl)} alt={work.name} />
               <motion.div
                 whileHover={{ opacity: [0, 1] }}
                 transition={{
                   duration: 0.25,
-                  ease: "easeInOut",
+                  ease: 'easeInOut',
                   staggerChildren: 0.5,
                 }}
-                className="app__work-hover app__flex"
+                className='app__work-hover app__flex'
               >
-                <a href={work.projectLink} target="_blank" rel="noreferrer">
+                <a href={work.projectLink} target='_blank' rel='noreferrer'>
                   <motion.div
                     whileInView={{ scale: [0, 1] }}
                     whileHover={{ scale: [1, 0.8] }}
                     transition={{
                       duration: 0.35,
                     }}
-                    className="app__flex"
+                    className='app__flex'
                   >
                     <AiFillEye />
                   </motion.div>
                 </a>
-                <a href={work.codeLink} target="_blank" rel="noreferrer">
+                <a href={work.codeLink} target='_blank' rel='noreferrer'>
                   <motion.div
                     whileInView={{ scale: [0, 1] }}
                     whileHover={{ scale: [1, 0.8] }}
                     transition={{
                       duration: 0.45,
                     }}
-                    className="app__flex"
+                    className='app__flex'
                   >
                     <AiFillGithub />
                   </motion.div>
                 </a>
               </motion.div>
             </div>
-            <div className="app__work-content app__flex">
-              <h4 className="bold-text">{work.title}</h4>
-              <p className="p-text" style={{ marginTop: 10 }}>
+            <div className='app__work-content app__flex'>
+              <h4 className='bold-text'>{work.title}</h4>
+              <p className='p-text' style={{ marginTop: 10 }}>
                 {work.description}
               </p>
-              <div className="app__work-tag app__flex">
-                <p className="p-text">{work.tags[0]}</p>
+              <div className='app__work-tag app__flex'>
+                <p className='p-text'>{work.tags[0]}</p>
               </div>
             </div>
           </div>
         ))}
       </motion.div>
     </>
-  );
+  )
 };
 
 export default AppWrap(Projects, "Projects");
